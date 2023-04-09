95ad2f25ef34dcc71fc11888831fca37597648b7
hoony0608 <rhdqndyd0608@gmail.com>
2023-04-09 13:07:41 +0900

    mvc와 템플릿 엔진

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -14,6 +15,19 @@ public class HelloController {
         // hello.html 문서에서 받아온 model에 th:text data라는 키에 "hello!"를 할당한다.
         return "hello";
         // hello 문서를 보여줌
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMVC(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("test-mvc")
+    public String practice(@RequestParam("test") String test, @RequestParam("test2") String test2, Model model) {
+        model.addAttribute("test", test);
+        model.addAttribute("test2", test2);
+        return "test-template";
     }
 }
 
