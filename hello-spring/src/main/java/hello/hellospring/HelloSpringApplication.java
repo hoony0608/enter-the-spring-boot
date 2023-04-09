28fe6c506edcf81424fcb890b095e6c7dd503d57
hoony0608 <rhdqndyd0608@gmail.com>
2023-04-09 09:07:09 +0900

    라이브러리 살펴보기

--- hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -3,7 +3,7 @@ package hello.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 스프링부트를 내장된 톰캣 서버에 띄움
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
