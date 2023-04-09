618abc9b9720c319ceb59128f6ed4ce680dbfd0c
hoony0608 <rhdqndyd0608@gmail.com>
2023-04-09 09:35:26 +0900

    view 환경설정

---
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,18 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        // hello url에 들어가면 스프링에서 model을 할당해 메소드 실행
+        model.addAttribute("data", "nemo!!!");
+        // hello.html 문서에서 받아온 model에 th:text data라는 키에 "hello!"를 할당한다.
+        return "hello";
+        // hello 문서를 보여줌
+    }
+}
